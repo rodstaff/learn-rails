c969ab1de2943d387ba8fb8d9065ee7d46fdaa66
rodk <rod2010rod2010@hotmail.com>
2016-06-04 01:36:49 -0400

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,6 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon!'
 #   render 'visitors/new'   #equivlanet code inherited from ApplicationController Class
                             # Rails Magic!  hidden deep in Rails code library
   end
