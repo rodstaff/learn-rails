76747a371d3222ba4eb86a2f76bf2d48fb4df0f8
rodk <rod2010rod2010@hotmail.com>
2016-06-03 23:39:20 -0400

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon!'
 #   render 'visitors/new'   #equivlanet code inherited from ApplicationController Class
                             # Rails Magic!  hidden deep in Rails code library
   end
