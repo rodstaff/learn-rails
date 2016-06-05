8d73fde8fe6080af6e5a95549527535e48975288
rodk <rod2010rod2010@hotmail.com>
2016-06-05 14:43:23 -0400

    email production.rb corrected

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,13 @@ Rails.application.configure do
  config.action_mailer.smtp_settings = {
    address: "smtp.sendgrid.net",
    port: 587,
-   domain: Rails.application.secrets.domain_name,
+   domain: Rails.application.secrets.DOMAIN_NAME,
    authentication: "plain",
    user_name: Rails.application.secrets.SENDGRID_USERNAME,
    password: Rails.application.secrets.SENDGRID_PASSWORD
  }
  # ActionMailer Config
- config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+ config.action_mailer.default_url_options = { :host => Rails.application.secrets.DOMAIN_NAME }
  config.action_mailer.delivery_method = :smtp
  config.action_mailer.perform_deliveries = true
  config.action_mailer.raise_delivery_errors = false
